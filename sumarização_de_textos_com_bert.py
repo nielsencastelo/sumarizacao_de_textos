0ccc37696af62cf56c82c07149a79686b572d800
Nielsen Castelo Damasceno Dantas <39357322+nielsencastelo@users.noreply.github.com>
2023-07-10 17:21:47 -0300

    Update sumarização_de_textos_com_bert.py

--- sumarização_de_textos_com_bert.py
+++ sumarização_de_textos_com_bert.py
@@ -7,21 +7,30 @@
 
 from transformers import BartTokenizer, BartForConditionalGeneration
 
-def summarize_text(text):
+def resumir_texto(texto):
     # Inicializar o tokenizador e o modelo BART pré-treinado
     tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-mnli')
     model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-mnli')
 
     # Preprocessar o texto
-    inputs = tokenizer.encode(text, return_tensors='pt')
+    inputs = tokenizer.encode(texto, return_tensors='pt')
 
-    # Gerar a sumarização
-    summary_ids = model.generate(inputs, max_length=150, min_length=40, num_beams=4, early_stopping=True)
-    summary = tokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True)
+    # Gerar o resumo
+    summary_ids = model.generate(inputs, max_length=50, min_length=10, num_beams=4, early_stopping=True)
+    resumo = tokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True)
 
-    return summary
+    return resumo
 
 # Exemplo de uso
-text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Mauris scelerisque tincidunt metus ac feugiat. Sed sit amet nisl id odio vestibulum accumsan. Donec iaculis finibus luctus. Pellentesque a velit cursus, bibendum lorem vel, fermentum sapien."
-summary = summarize_text(text)
-print(summary)
+texto_original = """A inteligência artificial é a inteligência similar à humana máquinas. 
+                    Definem como o estudo de agente artificial com inteligência. 
+                    Ciência e engenharia de produzir máquinas com inteligência. 
+                    Resolver problemas e possuir inteligência. 
+                    Relacionada ao comportamento inteligente. 
+                    Construção de máquinas para raciocinar. 
+                    Aprender com os erros e acertos. 
+                    Inteligência artificial é raciocinar nas situações do cotidiano."""
+
+resumo = resumir_texto(texto_original)
+print(resumo)
+
